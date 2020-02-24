cb885d4df7293967debc51b916d6dffccf473305
Panagiotis <p.nomikos@meazon.com>
2020-02-24 19:32:37 +0200

    Updates

--- Assets/Scripts/AttackTrigger.cs
+++ Assets/Scripts/AttackTrigger.cs
@@ -6,11 +6,10 @@ public class AttackTrigger : MonoBehaviour
 {
     public int damage = 20;
     private void OnTriggerEnter2D(Collider2D col)
-    {
+    { 
         if (col.isTrigger != true && col.CompareTag("Enemy"))
         {
             col.SendMessageUpwards("Damage", damage);
-            Debug.Log("Dealt damage");
         }
         gameObject.GetComponent<Collider2D>().enabled = false;
     }
