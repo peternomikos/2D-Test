35057e6b206c6cb963c8538acf05b2ff3b4a31e9
Panagiotis <p.nomikos@meazon.com>
2020-02-24 19:30:17 +0200

    Fixed the camera

--- Assets/Scripts/Camera_Follow.cs
+++ Assets/Scripts/Camera_Follow.cs
@@ -6,29 +6,20 @@ public class Camera_Follow : MonoBehaviour
 { 
     public float speed = 15f;
     public float minDistance;
-    public GameObject target;
+    public Transform player;
     public Vector3 offset;
 
-    private Vector3 targetPos;
 
     // Use this for initialization
     void Start()
     {
-        targetPos = transform.position;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target)
-        {
-            Vector3 posNoZ = transform.position + offset;
-            Vector3 targetDirection = (target.transform.position - posNoZ);
-            float interpVelocity = targetDirection.magnitude * speed;
-            targetPos = (transform.position) + (targetDirection.normalized * interpVelocity * Time.deltaTime);
-            transform.position = Vector3.Lerp(transform.position, targetPos, 0.25f);
-
-        }
+        transform.position = new Vector3(player.position.x + offset.x, 0, player.position.z + offset.z);
     }
 }
 
